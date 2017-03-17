d531751fb4dbaf6583029b2dfc8a9b9d54e95ebb
Edie <ehze@live.com>
2017-03-16 22:23:15 -0700

    doing stuff in R

---
+++ machine_learning.R
@@ -0,0 +1,11 @@
+# EDIE ESPEJO
+# setwd("/Users/EDIE/Box Sync/GitThings/project141b")
+
+# READ IN DATA
+predictions = read.csv("predictions.csv")
+head(predictions)
+
+wordbags = read.csv("blobs.csv")
+head(wordbags)
+
+
